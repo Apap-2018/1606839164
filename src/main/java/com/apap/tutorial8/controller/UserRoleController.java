ae6234a455e7e8270d939d68ec6b9cfb30a11f7d
Aldo <aldobagus@rocketmail.com>
2018-11-07 20:40:25 +0700

    selesai latihan tutorial 8

---
+++ src/main/java/com/apap/tutorial8/controller/UserRoleController.java
@@ -0,0 +1,45 @@
+package com.apap.tutorial8.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.apap.tutorial8.model.UserRoleModel;
+import com.apap.tutorial8.service.UserRoleService;
+
+@Controller
+@RequestMapping("/user")
+public class UserRoleController {
+	@Autowired
+	private UserRoleService userService;
+	
+	@RequestMapping(value = "/addUser", method = RequestMethod.POST)
+	private String addUserSubmit(@ModelAttribute UserRoleModel user) {
+		userService.addUser(user);
+		return "home";
+	}
+
+	@RequestMapping(value = "/updatePassword")
+	private String updatePasswordSubmit(Model model, @ModelAttribute UserRoleModel tempUser, String newPassword, String confirmationPassword) {
+		UserRoleModel realUser = userService.getUserByUsername(tempUser.getUsername());
+		
+		if (userService.isMatch(tempUser.getPassword(), realUser.getPassword())) {
+			if (newPassword.equals(confirmationPassword)) {
+				realUser.setPassword(newPassword);
+				userService.addUser(realUser);
+				model.addAttribute("msg", "Ubah Password Berhasil");
+			}
+			else {
+				model.addAttribute("msg", "Konfirmasi Password Salah");
+			}
+		}
+		else {
+			model.addAttribute("msg", "Password lama salah.");
+		}
+		
+		return "home";
+	}
+}
